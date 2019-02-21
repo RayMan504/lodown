d92379b631890c25d772390043d29eff68d89b38
Uraeus Millet <uraeusmillet@gmail.com>
2019-02-21 02:24:34 +0000

    (lodown) build map

--- index.js
+++ index.js
@@ -22,3 +22,28 @@ function each(collection, action) {
     }
 }
 module.exports.each = each;
+
+/**
+ * map: iterates over a collection (Array or Object) and applies a transform function which modifies
+ * each value in the collection. returns a new array of modified values
+ * 
+ * @Param {Array or Object}: collection to loop over
+ * @Param {Anonymous Function}: function that transforms each value when called
+ * 
+ */
+function map(collection, transform) {
+    //create new output array to store transformed values
+    var transformed = [];
+    // implement each to iterate over the collection
+    each(collection, (value, index, collection) => {
+        // call transform callback function on the value, index, and collection
+        //push results of transform function call to new output array
+        transformed.push(transform(value, index, collection));
+    })
+    //return array of transformed values
+    return transformed;
+}
+
+module.exports.map = map;
+
+
