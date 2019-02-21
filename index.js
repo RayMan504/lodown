94ad12c42a708d8a116456e37837056317851508
Uraeus Millet <uraeusmillet@gmail.com>
2019-02-21 02:39:14 +0000

    (lodown) build filter

--- index.js
+++ index.js
@@ -39,11 +39,37 @@ function map(collection, transform) {
         // call transform callback function on the value, index, and collection
         //push results of transform function call to new output array
         transformed.push(transform(value, index, collection));
-    })
+    });
     //return array of transformed values
     return transformed;
 }
 
 module.exports.map = map;
+
+/**
+ * filter: iterates over a collection (Array or Object) and returns a boolean value
+ * based on result of conditional statement with test function call. returns new array of truthy values if test
+ * function call on value, index, and collection is truthy. 
+ * 
+ * @Param {Array or Object}: collection to loop over
+ * @Param {Anonymous function}: function to test values
+ * 
+ */
+function filter(collection, test) {
+    //create array output to store truthy values
+    var passed = [];
+    //use each to iterate collection
+    each(collection, (value, index, collection) => {
+        //create conditional statement to test for truthy values on test function call
+        //if test call on value, index, and collection evaluates as true
+        if(test(value, index, collection)) {
+            //push values to new array
+            passed.push(value);
+        }
+    })
+    //return new array of truthy values
+    return true;
+}
+module.exports.filter = filter;
 
 
